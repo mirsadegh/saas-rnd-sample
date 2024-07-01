27c150f9a7e6f43995ef56f4d78ebe0bc452b2d0
sadegh <mirsadeghporbehnam831@gmail.com>
2024-07-01 11:26:56 +0330

    Updated views to change order

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -11,6 +11,7 @@ def home_page_view(request, *args, **kwargs):
     page_qs = PageVisit.objects.filter(path=request.path)
 
     my_title = "My Page"
+    html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count":page_qs.count(),
@@ -18,7 +19,7 @@ def home_page_view(request, *args, **kwargs):
         "total_visit_count": qs.count(),
     }
     
-    html_template = "home.html"
+    
     PageVisit.objects.create(path=request.path)
 
     return render(request, html_template, my_context)
