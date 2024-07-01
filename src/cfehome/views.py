9f911693e11fbe46ec78cee6d1d1351032685830
sadegh <mirsadeghporbehnam831@gmail.com>
2024-07-01 11:13:20 +0330

    hello world msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,45 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count":page_qs.count(),
+        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "total_visit_count": qs.count(),
+    }
+    
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+
+    return render(request, html_template, my_context)
+
+
+
+def home_old_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = f"""
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <body>
+            <h1>{page_title} is anything?</h1>
+        </body>
+        </html>
+
+""".format(my_context)
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
